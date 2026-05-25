c2e54c3c4cc6054a44d18869884c8a794011a3d2
savasanisujal1 <savasanisujal1@gmail.com>
2026-05-25 13:00:34 +0530

    added API rate limiting (throttle) and Postman collection export button in Swagger UI

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,24 @@
 
 use App\Http\Controllers\SwaggerTestController;
 use App\Http\Controllers\Api\UserController;
-use Illuminate\Support\Facades\Route;   
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 
+Route::middleware('throttle:api')->group(function () {
+    Route::get('/test', [SwaggerTestController::class, 'test']);
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::post('/products', [ProductController::class, 'store']);
+});
 
-Route::get('/test', [SwaggerTestController::class, 'test']);
-Route::get('/users', [UserController::class, 'index']);
+Route::get('/postman-export', function () {
+    $path = storage_path('api-docs/api-docs.json');
 
-Route::get('/products', [ProductController::class, 'index']);
-Route::post('/products', [ProductController::class, 'store']);
+    if (!file_exists($path)) {
+        return response()->json(['error' => 'File not found'], 404);
+    }
+
+    return response()->download($path, 'Laravel12_Postman_Collection.json', [
+        'Content-Type' => 'application/json',
+    ]);
+});
